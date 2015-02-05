90aad794a63fa8427fe2fcc4e8a44ccf8733ea38
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-02-05 17:37:41 -0500

    adds base project

---
+++ Queue-sadilla/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Queue-sadilla
+//
+//  Created by Chris Gonzales on 2/5/15.
+//  Copyright (c) 2015 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
