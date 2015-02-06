971cabe3b167e6c26f7a08dd0b7fc619e59e4406
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-02-06 09:52:35 -0500

    adds tests

--- Queue-sadillaTests/FISQueueSpec.m
+++ Queue-sadillaTests/FISQueueSpec.m
@@ -8,31 +8,113 @@
 
 #import "Specta.h"
 #import "FISQueue.h"
-
+#define EXP_SHORTHAND
+#import <Expecta.h>
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
 
 SpecBegin(FISQueue)
 
 describe(@"FISQueue", ^{
-    
-    beforeAll(^{
-
+  __block FISQueue *queue = [[FISQueue alloc] init];
+  __block NSMutableArray *queueBackingArray = [queue performSelector:@selector(queue)];
+  
+  beforeEach(^{
+    [queueBackingArray removeAllObjects];
+  });
+  
+  describe(@"checking if the queue is empty", ^{
+    context(@"when the queue is empty", ^{
+      it(@"should return true",^{
+        FISQueue *emptyQueue = [[FISQueue alloc] init];
+        expect(emptyQueue.isEmpty).to.beTruthy();
+      });
     });
     
-    beforeEach(^{
-
+    context(@"when the queue is not empty", ^{
+      it(@"should return false",^{
+        [queueBackingArray addObject:@1];
+        expect(queue.isEmpty).to.beFalsy();
+      });
+    });
+  });
+  
+  describe(@"adding an object to the queue", ^{
+    it(@"should add an object to the queue",^{
+      [queue enqueue:@2];
+      expect(queueBackingArray.count).to.equal(1);
+    });
+  });
+  
+  describe(@"removing an object from the queue", ^{
+    context(@"when there are items in the queue", ^{
+      beforeEach(^{
+        [queueBackingArray addObjectsFromArray:@[@3, @4]];
+      });
+      
+      it(@"should remove the first item in the queue",^{
+        [queue dequeue];
+        expect([queueBackingArray indexOfObject:@3]).to.equal(NSNotFound);
+      });
+      
+      it(@"should return the dequeued object",^{
+        NSNumber *dequeuedObject = [queue dequeue];
+        expect(dequeuedObject).to.equal(@3);
+      });
     });
     
-    it(@"", ^{
-
-    });  
-    
-    afterEach(^{
-
+    context(@"when there are no items in the queue", ^{
+      it(@"should return nil",^{
+        FISQueue *emptyQueue = [[FISQueue alloc] init];
+        expect([emptyQueue dequeue]).to.beNil();
+      });
     });
     
-    afterAll(^{
-
+  });
+  
+  describe(@"asking for the number if items in the queue", ^{
+    context(@"when there are items in the queue", ^{
+      it(@"should return the number of items",^{
+        [queueBackingArray addObjectsFromArray:@[@5,@6,@7]];
+        expect(queue.size).to.equal(3);
+      });
     });
+    
+    context(@"when there are no items in the queue", ^{
+      it(@"should return 0",^{
+        FISQueue *emptyQueue = [[FISQueue alloc] init];
+        expect(emptyQueue.size).to.equal(0);
+      });
+    });
+  });
+  
+  describe(@"getting the next object in the queue", ^{
+    context(@"when there are items in the queue", ^{
+      beforeEach(^{
+        [queueBackingArray addObjectsFromArray:@[@8, @9]];
+      });
+      
+      it(@"should return the next item in the queue",^{
+        NSNumber *peekedNumber = [queue peek];
+        expect(peekedNumber).to.equal(@8);
+      });
+      
+      it(@"should not change the queue",^{
+        NSArray *itemsBeforePeek = [queueBackingArray copy];
+        [queue peek];
+        expect(queueBackingArray).to.equal(itemsBeforePeek);
+      });
+    });
+    
+    context(@"when there are no items in the queue", ^{
+      it(@"should return nil",^{
+        FISQueue *emptyQueue = [[FISQueue alloc] init];
+        expect([emptyQueue peek]).to.beNil();
+      });
+    });
+  });
 });
 
 SpecEnd
+#pragma clang diagnostic pop
+
